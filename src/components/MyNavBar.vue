2687662458a5e0894f462be37ea490625d3199b3
Andrew Bell <andrew@recursivechaos.com>
2020-04-13 14:38:38 -0500

    Add GH link, icons, and GH deployment

--- src/components/MyNavBar.vue
+++ src/components/MyNavBar.vue
@@ -1,10 +1,13 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="info">
+  <b-navbar toggleable="sm" type="dark" variant="info">
     <b-navbar-brand href="#">Portfolio Planner</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="https://github.com/AndrewBell/portfolio-planner" target="_blank">GitHub</b-nav-item>
+      </b-navbar-nav>
       <b-navbar-nav>
         <b-nav-item href="https://www.recursivechaos.com/" target="_blank">About the Creator</b-nav-item>
       </b-navbar-nav>
